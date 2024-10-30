47b35100b25ac1e1a728c53a40d92692131dcec6
DanielWar01 <danielalejoguerra@gmail.com>
2024-10-30 00:55:17 -0500

    Initial Commit

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto{
+    @IsString()
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+
+    constructor(private userService: UserService){}
+
+    @Get(':id')
+    async getUserProfile(@Param("id") id : number){
+        return await this.userService.findById(id);
+    }
+}
